7a9c9e53396246b86a0f83a4b1b8b52394294fff
Zeka97 <zekafrombtm@hotmail.com>
2024-01-15 19:06:10 +0100

    productslist

--- src/pages/Home/Products.js
+++ src/pages/Home/Products.js
@@ -1,8 +1,13 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useQuery } from "react-query";
 import { getAllProducts } from "../../api";
+import Input from "../../components/Input/Input";
 
 const Products = () => {
+  const [limit, setLimit] = useState(10);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   const { currentUser } = JSON.parse(localStorage.getItem("user"));
   const { data, error, isError, isFetching, isFetched, isLoading, isSuccess } =
     useQuery("getProducts", () => getAllProducts(currentUser));
@@ -35,9 +40,71 @@ const Products = () => {
     return uniqueProductsMap.values;
   }, [data?.products]);
 
-  console.log(filteredData.filter((item) => item.name === "Product name1"));
+  console.log(filteredData);
+  console.log(
+    filteredData?.filter((item) => {
+      const regex = new RegExp(`.*${name}\\w+`);
+      var matchesRegex = regex.test(item.name);
+      return matchesRegex;
+    })
+  );
 
-  return <div></div>;
+  console.log(name, price);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-around">
+        <Input
+          label="Name"
+          type="text"
+          name="name"
+          onChange={(e) => setName(e)}
+        />
+        <Input
+          label="Price"
+          type="number"
+          name="price"
+          onChange={(e) => setPrice(e)}
+        />
+      </div>
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-4">Product Table</h2>
+        <table className="min-w-full bg-white border border-gray-300">
+          <thead>
+            <tr>
+              <th className="py-2 px-4 border-b">ID</th>
+              <th className="py-2 px-4 border-b">Name</th>
+              <th className="py-2 px-4 border-b">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData
+              ?.filter((item) => {
+                const regex = new RegExp(`.*${name}.+|${name}`);
+                var matchesRegexName = regex.test(item.name);
+                const regex2 = new RegExp(`^${price}.+|^${price}$`);
+                var matchesRegexPrice = regex2.test(item.price);
+                console.log(matchesRegexPrice);
+                return matchesRegexName && matchesRegexPrice;
+              })
+              .map((product, index) => (
+                <tr key={product.id}>
+                  <td className="py-2 px-4 border-b">{product.id}</td>
+                  <td className="py-2 px-4 border-b">{product.name}</td>
+                  <td className="py-2 px-4 border-b">{product.price}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+
+        <select onChange={(e) => setLimit(e.target.value)}>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
